0e12635f514eaa1fef7d480b0b4950056526c799
Karita Shuhei <shoony.1015@gmail.com>
2021-01-10 18:24:32 +0900

    Initial Commit

---
+++ appFormat1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  appFormat1
+//
+//  Created by 刈田修平 on 2021/01/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
